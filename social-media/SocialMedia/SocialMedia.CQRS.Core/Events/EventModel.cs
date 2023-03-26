3e1742f615707eee17e1ec057819ab7f441e1af1
martinbalucha <martin.balucha@outlook.com>
2023-03-26 18:37:43 +0200

    Created EventModel

---
+++ social-media/SocialMedia/SocialMedia.CQRS.Core/Events/EventModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialMedia.CQRS.Core.Events;
+
+public class EventModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; init; }
+    public DateTimeOffset TimeStamp { get; set; }
+    public Guid AggregateId { get; set; }
+    public string AggregateType { get; set; }
+    public int Version { get; set; }
+    public string EventType { get; set; }
+    public required IEvent EventData { get; set; }
+}
